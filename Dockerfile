f22f784f1fc20259281dc810083d97f4770609c1
GreatZeve <cesar2407113@hybridge.education>
2026-09-08 05:07:44 -0600

    Primer commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+
+FROM python:3.13.7
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install -r requirements-dev.txt
+
+CMD ["python", "main.py"]
---
+++ generator/password_generator.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = (
+        string.ascii_lowercase +
+        string.ascii_uppercase +
+	    string.digits +
+	    string.punctuation
+    )
+
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+def has_uppercase(password):
+    return any(c.isupper() for c in password)
+
+def has_number(password):
+    return any(c.isdigit() for c in password)
+
+def has_symbol(password):
+    return any(c in string.punctuation for c in password)
+
+def validate_password(password):
+    return (
+        has_uppercase(password) and
+        has_number(password) and
+        has_symbol(password)
+    )
+
+def generate_secure_password(length=12):
+    while True:
+        password = generate_password(length)
+        if validate_password(password):
+            return password
